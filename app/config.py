3add73ec3a1f250fefba8c866d69ce86a49381fe
donky16 <zhudengforever@gmail.com>
2019-06-10 12:57:54 +0800

    update select course

--- app/config.py
+++ app/config.py
@@ -4,6 +4,7 @@ appid = 'wx607b7ccda2701603'
 secret = '24dce280d0ecfd0488b9c0473b662159'
 
 start_time = '2018-09-17 00:00:00'   # 开学时间
-
+xn = '2019'
+xnxq = '2019-0'  # 0-夏季学期  1-秋季学期  2-春季学期
 SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost:3306/wx?charset=utf8'
 SQLALCHEMY_TRACK_MODIFICATIONS = True
